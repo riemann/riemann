5b90f832bf27ca5adeaf06b89e9847ce2cbde604
Aphyr <aphyr@aphyr.com>
2012-02-16 17:17:41 -0800

    logging/init takes bare options now, e.g.
    (logging/init :file "foo").
    
    I'm zeroing in on a convention that things called from the config file,
    where possible use the bare options convention. Not streams--makes it
    hard to distinguish children from options.

--- src/reimann/logging.clj
+++ src/reimann/logging.clj
@@ -27,28 +27,27 @@
   "Initialize log4j. You will probably call this from the config file. Options:
 
   :file   The file to log to."
-  ([] (init {}))
-  ([opts]
-    (let [filename (or (:file opts) "reimann.log")
-          rolling-policy (doto (TimeBasedRollingPolicy.)
-                           (.setActiveFileName filename)
-                           (.setFileNamePattern 
-                             (str filename ".%d{yyyy-MM-dd}.gz"))
-                           (.activateOptions))
-          log-appender (doto (RollingFileAppender.)
-                         (.setRollingPolicy rolling-policy)
-                         (.setLayout reimann-layout)
-                         (.activateOptions))]
-      (doto (Logger/getRootLogger)
-        (.removeAllAppenders)
-        (.addAppender log-appender)
-        (.addAppender (ConsoleAppender. reimann-layout))))
-    (. (Logger/getRootLogger) (setLevel Level/INFO))
+  [& { :keys [file] }]
+  (let [filename (or file "reimann.log")
+        rolling-policy (doto (TimeBasedRollingPolicy.)
+                         (.setActiveFileName filename)
+                         (.setFileNamePattern 
+                           (str filename ".%d{yyyy-MM-dd}.gz"))
+                         (.activateOptions))
+        log-appender (doto (RollingFileAppender.)
+                       (.setRollingPolicy rolling-policy)
+                       (.setLayout reimann-layout)
+                       (.activateOptions))]
+    (doto (Logger/getRootLogger)
+      (.removeAllAppenders)
+      (.addAppender log-appender)
+      (.addAppender (ConsoleAppender. reimann-layout))))
+  (. (Logger/getRootLogger) (setLevel Level/INFO))
 
-    (set-level "reimann.client" Level/DEBUG)
-    (set-level "reimann.server" Level/DEBUG)
-    (set-level "reimann.streams" Level/DEBUG)
-    (set-level "reimann.graphite" Level/DEBUG)))
+  (set-level "reimann.client" Level/DEBUG)
+  (set-level "reimann.server" Level/DEBUG)
+  (set-level "reimann.streams" Level/DEBUG)
+  (set-level "reimann.graphite" Level/DEBUG))
 
 ; Not sure where he intended this to go....
 (defn- add-file-appender [loggername filename]
