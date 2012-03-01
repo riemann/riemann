1c7a6759b1cc3c9b3260bc5f7b573e389a0c9b74
Aphyr <aphyr@aphyr.com>
2012-03-01 00:40:25 -0800

    Graphite: transforms percentile services into nice paths.

--- src/riemann/graphite.clj
+++ src/riemann/graphite.clj
@@ -3,20 +3,45 @@
   (:import [java.net Socket])
   (:import [java.io Writer])
   (:import [java.io OutputStreamWriter])
-  (:use [clojure.string :only [split join]])
+  (:use [clojure.string :only [split join replace]])
   (:use clojure.tools.logging)
   (:use riemann.common))
 
+(defn graphite-path-basic
+  "Constructs a path for an event. Takes the hostname fqdn, reversed,
+  followed by the service, with spaces converted to dots."
+  [event]
+  (let [service (:service event)
+        host (:host event)
+        split-service (if service (split service #" ") [])
+        split-host (if host (split host #"\.") [])]
+     (join "." (concat (reverse split-host) split-service))))
+
+(defn graphite-path-percentiles
+  "Like graphite-service-basic, but also converts trailing decimals like 0.95
+  to 95."
+  [event]
+  (graphite-path-basic
+    (if-let [service (:service event)]
+      (assoc event :service
+             (replace service
+                      #"([\d\.]+)$"
+                      (fn [[_ x]] (str (int (* 100 (read-string x)))))))
+      event)))
+
 (defn graphite 
   "Returns a function which accepts an event and sends it to Graphite.
-  Constructs service names by taking the host (reversed, e.g. \"web.pdx\" ->
-  \"lax.web\"), and service (with spaces converted to dots). Silently eats
-  events when graphite is down. Attempts to reconnect automatically every five
-  seconds. Use:
+  Silently eats events when graphite is down. Attempts to reconnect
+  automatically every five seconds. Use:
   
-  (graphite {:host \"graphite.local\" :port 2003})"
+  (graphite {:host \"graphite.local\" :port 2003})
+  
+  Set :path (fn [event] some-string) to change the path for each event. Uses
+  graphite-path-percentiles by default."
   [opts]
-  (let [opts (merge {:host "localhost" :port 2003} opts)
+  (let [opts (merge {:host "localhost" 
+                     :port 2003
+                     :path graphite-path-percentiles} opts)
         sock (ref nil)
         out  (ref nil)
         close (fn []
@@ -35,19 +60,8 @@
                   (ref-set sock (Socket. (:host opts) (:port opts)))
                   (ref-set out (OutputStreamWriter.
                                  (.getOutputStream (deref sock))))))
+        path (:path opts)]
         
-        ; Transform events into names.
-        ; :host "foo.com"
-        ; :service "thing 1"
-        ; -> "com.foo.thing.1"
-        name (fn [event]
-               (let [service (:service event)
-                     host (:host event)
-                     split-service (if service (split service #" ") [])
-                     split-host (if host (split host #"\.") [])]
-                  (join "." (concat (reverse split-host) split-service))))
-        ]
-
     ; Try to connect immediately
     (try
       (open)
@@ -56,7 +70,7 @@
 
     (fn [event]
       (when (:metric event)
-        (let [string (str (join " " [(name event) 
+        (let [string (str (join " " [(path event) 
                                      (float (:metric event))
                                      (int (:time event))])
                           "\n")]
--- test/riemann/test/graphite.clj
+++ test/riemann/test/graphite.clj
@@ -1,7 +1,10 @@
 (ns riemann.test.graphite
   (:use riemann.graphite)
   (:use riemann.common)
+  (:require [riemann.logging :as logging])
   (:use clojure.test))
+
+(logging/init)
 
 (deftest ^:integration graphite-test
          (let [g (graphite {})]
