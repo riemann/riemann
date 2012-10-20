f2331daff34e36d60cee436d5f126fb9aa29800e
Pierre-Yves Ritschard <pyr@spootnik.org>
2012-10-20 10:13:01 +0200

    Switch pubsub registry to an atom.
    I suppose later on a protocol could be implemented for this,
    allowing interfacing with external pubsub systems.

--- src/riemann/pubsub.clj
+++ src/riemann/pubsub.clj
@@ -2,9 +2,7 @@
   "Provides publish-subscribe handling of events. Publishers push events onto a
   channel, which has n subscribers. Each subscriber subscribes to a channel
   with an optional predicate function. Events which match the predicate are
-  sent to the subscriber."
-
-  (:use riemann.common))
+  sent to the subscriber.")
 
 ; Registry:
 ;   channel1:
@@ -18,13 +16,13 @@
   "Returns a new pubsub registry, which tracks which subscribers are
   listening to which channels."
   []
-  {:channels (ref {})
-   :last-sub-id (ref 0)})
+  (atom {:channels    {}
+         :last-sub-id 0}))
 
 (defn publish
   "Publish an event to the given channel in a registry."
   [registry channel event]
-  (let [channels (deref (:channels registry))]
+  (let [channels (:channels @registry)]
     (doseq [[id f] (channels channel)]
       (f event))))
 
@@ -32,11 +30,13 @@
   "Subscribe to the given channel in a registry with f, which is called with
   each event that arrives on that channel. Returns an ID for the subscription."
   [registry channel f]
-  (let [channels (:channels registry)]
-    (dosync
-      (let [sub-id (alter (:last-sub-id registry) inc)]
-        (alter channels assoc-in [channel sub-id] f)
-        sub-id))))
+  (let [sub-id          (-> @registry :last-sub-id inc)
+        inner-subscribe (fn [registry channel f]
+                          (-> registry
+                              (assoc-in [:channels channel sub-id] f)
+                              (assoc :last-sub-id sub-id)))]
+    (swap! registry inner-subscribe channel f)
+    sub-id))
 
 (defn dissoc-in
   "Dissociates an entry from a nested associative structure returning a new
@@ -56,13 +56,8 @@
   "Unsubscribe from the given registry by id. If you provide a channel to
   unsubscribe from, O(1). If you provide only the id, O(channels)."
   ([registry channel id]
-   (let [channels (:channels registry)]
-     (dosync
-       (alter channels dissoc-in [channel id]))))
+     (swap! registry dissoc-in [:channels channel id]))
 
   ([registry id]
-   (let [channels (:channels registry)]
-     (dosync
-       (ref-set channels
-                (into {} (for [[channel channel-subs] (deref channels)]
-                           [channel (dissoc channel-subs id)])))))))
+     (swap! registry update-in [:channels]
+            #(reduce merge (for [[chan subs] %] {chan (dissoc subs id)})))))
