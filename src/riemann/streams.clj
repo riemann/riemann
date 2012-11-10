e5e9ac644c9b367b832fe245b5b6f88325dd3db6
Aphyr <aphyr@aphyr.com>
2012-11-10 14:25:41 -0800

    Document streams/tagged

--- src/riemann/streams.clj
+++ src/riemann/streams.clj
@@ -699,7 +699,7 @@
   (fn [event]
     (when (set/subset? (set tags) (set (:tags event)))
       (call-rescue event children))))
-(def tagged tagged-all)
+(def tagged "Alias for tagged-all" tagged-all)
 
 (defn tagged-any
   "Passes on events where any of tags are present.
