30d84f3dd648afcd1840cd546f4979aa9ef4e211
Aphyr <aphyr@aphyr.com>
2012-12-03 20:15:17 -0800

    Version 0.1.4

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 (defn relative-path [path]
     (expand-path (str (dirname *file*) "/" path)))
 
-(defproject riemann "0.1.4-SNAPSHOT"
+(defproject riemann "0.1.4"
   :description 
 "A network event stream processor. Intended for analytics, metrics, and alerting; and to glue various monitoring systems together."
   :url "http://github.com/aphyr/riemann"
