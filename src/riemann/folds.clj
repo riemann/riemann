c3d14265b2673be1eb48736b4b7d1346cf106bb4
Aphyr <aphyr@aphyr.com>
2013-02-25 23:10:58 -0800

    riemann.folds: total redesign of semantics.
    Most folds now come in a friendly (sum) and a strict (sum*) variety,
    intended to do sane, informative, correct things under common
    circumstances like being asked to reduce nil events or events with nil
    metrics. Should close #157, I believe. May do some fine-tuning before
    release.

--- src/riemann/folds.clj
+++ src/riemann/folds.clj
@@ -1,96 +1,159 @@
 (ns riemann.folds
-  "Functions for combining states."
+  "Functions for combining states.
+  
+  Folds usually come in two variants: a friendly version like sum, and a strict
+  version like sum*. Strict variants will throw when one of their events is
+  nil, missing a metric, or otherwise invalid. In typical use, however, you
+  won't *have* all the necessary information to pass on an event. Friendly
+  variants will do their best to ignore these error conditions where sensible,
+  returning partially complete events or nil instead of throwing.
+  
+  Called with an empty list, folds which would return a single event return
+  nil."
   (:use [riemann.common]))
 
 (defn sorted-sample-extract
   "Returns the events in seqable s, sorted and taken at each point p of points,
   where p ranges from 0 (smallest metric) to 1 (largest metric). 0.5 is the
-  median event, 0.95 is the 95th' percentile event, and so forth."
+  median event, 0.95 is the 95th' percentile event, and so forth. Ignores
+  events without a metric."
   [s points]
-  (if (empty? s)
-    '()
-    (let [sorted (sort-by :metric s)
-          n (count sorted)
-          extract (fn [point]
-                    (let [idx (min (dec n) (int (Math/floor (* n point))))]
-                      (nth sorted idx)))]
-      (map extract points))))
+  (let [sorted (sort-by :metric (filter :metric s))]
+    (if (empty? sorted)
+      '()
+      (let [n (count sorted)
+            extract (fn [point]
+                      (let [idx (min (dec n) (int (Math/floor (* n point))))]
+                        (nth sorted idx)))]
+        (map extract points)))))
 
 (defn sorted-sample
-  "Sample a sequence of events at points, return states with service remapped
-  to service + point. For instance, (sorted-sample events [0 1]) returns a
-  2-element seq of the smallest event and the biggest event, by metric. The
-  first has a service which ends in \" 0\" and the second one ends in \" 1\".
-  Useful for extracting histograms and percentiles."
+  "Sample a sequence of events at points. Returns events with service remapped
+  to (str service \" \" point). For instance, (sorted-sample events [0 1])
+  returns a 2-element seq of the smallest event and the biggest event, by
+  metric. The first has a service which ends in \" 0\" and the second one ends
+  in \" 1\".  Useful for extracting histograms and percentiles.
+  
+  When s is empty, returns an empty list."
   [s points]
-  (map (fn [point, event]
+  (map (fn [point event]
          (assoc event :service
                 (str (:service event) " " point)))
        points
        (sorted-sample-extract s points)))
 
+(defn non-nil-metrics
+  "Given a sequence of events, returns a compact sequence of their
+  metrics--that is, omits any events which are nil, or have nil metrics."
+  [events]
+  (keep (fn [event]
+          (when-not (nil? event)
+            (:metric event)))
+        events))
+
+(defn fold*
+  "Fold with a reduction function over metrics. Throws if any event or metric
+  is nil."
+  [f events]
+  (assoc (first events) :metric (reduce f (map :metric events))))
+
+(defn fold
+  "Fold with a reduction function over metrics. Ignores nil events and events
+  with nil metrics.
+  
+  If there are *no* non-nil events, returns nil."
+  [f events]
+  (when-let [e (some identity events)]
+    (assoc e :metric (reduce f (non-nil-metrics events)))))
+
+(defn fold-all
+  "Fold with a reduction function over metrics.
+
+  If the first event has a nil :metric, or if any remaining event is nil, or
+  has a nil metric, returns the first event, but with :metric nil and a
+  :description of the error.
+ 
+  If the first event is nil, returns nil."
+  [f events]
+  (when-let [e (first events)]
+    (try
+      (assoc e :metric (reduce f (map :metric events)))
+      (catch NullPointerException ex
+        (merge e
+               {:metric nil
+                :description "An event or metric was nil."})))))
+
+(defn sum*
+  "Adds events together. Sums metrics, merges into first of events."
+  [events]
+  (fold* + events))
+
 (defn sum
-  "Adds events together. Sums metric, merges into first of events."
+  "Adds events together. Sums metrics, merges into first event with a metric,
+  ignores nil events and nil metrics."
   [events]
-  (assoc (first events)
-         :metric
-         (reduce + (map :metric events))))
+  (fold + events))
 
-(defn difference
-  "Subtracts events. Returns the first event, with its metric reduced by the
-  metrics of all subsequent events."
-  [events]
-  (assoc (first events)
-         :metric
-         (reduce - (map :metric events))))
-
-(defn product
+(defn product*
   "Multiplies events. Returns the first event, with its metric multiplied by
   the metrics of all other events."
   [events]
-  (assoc (first events)
-         :metric
-         (reduce * (map :metric events))))
+  (fold* * events))
+
+(defn product
+  "Multiplies events. Returns the first event with a metric, with its metric
+  being the proiduct of all events with metrics."
+  [events]
+  (fold * events))
+
+(defn difference*
+  "Subtracts events. Returns the first event, with its metric reduced by the
+  metrics of all subsequent events."
+  [events]
+  (fold* - events))
+
+(defn difference
+  "Subtracts events. Returns the first event, with its metric reduced by the
+  metrics of all subsequent events with metrics. Returns nil if the first event
+  is nil, or its metric is nil."
+  [events]
+  (fold-all - events))
 
 (defn quotient*
-  "Divides events. Like quotient, but throws when any metric is nil or a
-  divisor is zero."
+  "Divides events. Returns the first event, with its metric multiplied by the
+  metrics of all subsequent events. Like quotient, but throws when any metric
+  is nil or a divisor is zero."
   [events]
-  (assoc (first events)
-         :metric
-         (reduce / (map :metric events))))
+  (fold* / events))
 
 (defn quotient
   "Divides events. Returns the first event, with its metric divided by the
-  product of the metrics of all subsequent events. If the first event's metric
-  is nil, or any later metric is zero or nil, returns :metric nil and
-  :description explaining the failure."
+  product of the metrics of all subsequent events."
   [events]
-  (try
-    (quotient* events)
-    (catch NullPointerException e
-      (merge (first events)
-             {:metric nil
-              :description "Can't divide nil metrics"}))
-    (catch ArithmeticException e
-      (merge (first events)
-             {:metric nil
-              :description "Can't divide by zero"}))))
+  (when-let [event (first events)]
+    (try
+      (fold-all / events) 
+      (catch ArithmeticException e
+        (merge event
+               {:metric nil
+                :description "Can't divide by zero"})))))
 
-(defn sloppy-quotient
+(defn quotient-sloppy
   "Like quotient, but considers 0/0 = 0. Useful for relative rates, when you
-  want the rate of change between x(t1) = 0 and x(t2) = 0 to be zero."
+  want the ratio of two constant values to be zero."
   [events]
-  (if (zero? (:metric (first events)))
+  (if (and (first events)
+           (zero? (:metric (first events))))
     (first events)
     (quotient events)))
 
 (defn mean
-  "Averages events together. Mean metric, merged into first of events."
+  "Averages events together. Mean metric, merged into first of events. Ignores
+  nil events and nil metrics."
   [events]
-  (assoc (first events)
-         :metric
-         (/ (reduce + (map :metric events)) (count events))))
+  (when-let [e (some identity events)]
+    (let [metrics (non-nil-metrics events)]
+      (assoc e :metric (/ (reduce + metrics) (count metrics))))))
 
 (defn median
   "Returns the median event from events, by metric."
@@ -100,9 +163,9 @@
 (defn minimum
   "Returns the minimum event, by metric."
   [events]
-  (apply min-key :metric events))
+  (apply min-key :metric (filter #(and % (:metric %)) events)))
 
 (defn maximum
   "Returns the maximum event, by metric."
   [events]
-  (apply max-key :metric events))
+  (apply max-key :metric (filter #(and % (:metric %)) events)))
--- test/riemann/test/folds.clj
+++ test/riemann/test/folds.clj
@@ -8,27 +8,84 @@
 (use-fixtures :once control-time!)
 (use-fixtures :each reset-time!)
 
+(deftest sorted-sample-test
+         (are [es e] (= (sorted-sample-extract es [0 0.5 1]) e)
+              []
+              []
+
+              [{:metric nil}]
+              []
+
+              [{:metric 1}]
+              [{:metric 1} {:metric 1} {:metric 1}]
+
+              [{:metric 2} {:metric 1}]
+              [{:metric 1} {:metric 2} {:metric 2}]
+
+              [{:metric 3} {:metric 1} {:metric 2}]
+              [{:metric 1} {:metric 2} {:metric 3}]
+              
+              [{:metric 6} {:metric 1} {:metric 2} {:metric 1} {:metric 1}]
+              [{:metric 1} {:metric 1} {:metric 6}]))
+
+(defn test-fold-common
+  [fold operator]
+  (are [es] (= (fold es) 
+               (assoc (first es)
+                      :metric
+                      (reduce operator (map :metric es))))
+       [{:metric 1 :a true}]
+       [{:metric 1 :a true} {:metric 2 :b true}]
+       [{:metric 1 :a true} {:metric 2 :b true} {:metric 7 :c true}]
+       [{:metric 1 :a true} {:metric 2 :b true} {:metric 7 :c true}
+        {:metric 7 :d true}]))
+
+(defn test-fold
+  "Tests a fold which is equivalent to operator."
+  [fold operator]
+  (is (nil? (fold [])))
+  (is (nil? (fold [nil])))
+  (is (= {:service "foo"
+          :metric (operator)}
+         (fold [{:service "foo" :metric nil} {}])))
+  (are [es] (= (fold es)
+              (assoc (first es)
+                     :metric
+                     (reduce operator
+                             (keep :metric es))))
+       [{:a true}]
+       [{:a true :metric 2} nil]
+       [{:a true :metric 2} nil {:b true :metric 7} {:c true}])
+  (test-fold-common fold operator))
+
+(defn test-fold-all
+  "Tests a fold which is equivalent to an operator where the first argument
+  is mandatory."
+  [fold operator]
+  (is (nil? (fold [])))
+  (is (nil? (fold [nil {:service "foo"}])))
+  (is (= {:service "foo"
+          :metric nil 
+          :description "An event or metric was nil."}
+         (fold [{:service "foo"} {:metric 2}])))
+  (is (= {:service "foo"
+          :metric nil
+          :description "An event or metric was nil."}
+         (fold [{:service "foo" :metric 2}
+                nil])))
+  (test-fold-common fold operator))
+
+(deftest sum-test
+         (test-fold sum +))
+
+(deftest product-test
+         (test-fold product *))
+
 (deftest difference-test
-         (are [es e] (= (difference es) e)
-              [{:metric 1 :a 2}]
-              {:metric 1 :a 2}
-
-              [{:metric 1 :a true} {:metric 2 :b true}]
-              {:metric -1 :a true}
-
-              [{:metric 10} {:metric -7} {:metric 2}]
-              {:metric 15}))
+         (test-fold-all difference -))
 
 (deftest quotient-test
-         (are [es e] (= (quotient es) e)
-              [{:metric 1 :a true}]
-              {:metric 1 :a true}
-
-              [{:metric 1 :a true} {:metric 2 :b true}]
-              {:metric 1/2 :a true}
-
-              [{:metric 10} {:metric -7} {:metric 2}]
-              {:metric (/ 10 -7 2)})
+         (test-fold-all quotient /)
 
          (testing "exceptions"
                   (is (= (quotient [{:service "hi" :metric 1} 
@@ -37,24 +94,14 @@
                          {:service "hi"
                           :metric nil
                           :description "Can't divide by zero"}))
-                  (is (= (quotient [{:service "hi"} {:metric 2}])
+                  (is (= (quotient [{:service "hi" :metric 2} {:service "bar"}])
                          {:service "hi"
                           :metric nil
-                          :description "Can't divide nil metrics"}))))
+                          :description "An event or metric was nil."}))))
 
-(deftest sloppy-quotient-test
-         (is (= (sloppy-quotient [{:metric 2} {:metric -3}])
+(deftest quotient-sloppy-test
+         (is (= (quotient-sloppy [{:metric 2} {:metric -3}])
                 {:metric -2/3}))
-         (is (= (sloppy-quotient [{:metric 0 :a true} {:metric 0}])
+         (is (= (quotient-sloppy [{:metric 0 :a true} {:metric 0}])
                 {:metric 0 :a true})))
 
-(deftest product-test
-         (are [es e] (= (product es) e)
-              [{:metric 1 :a true}]
-              {:metric 1 :a true}
-              
-              [{:metric 1 :a true} {:metric 2 :b true}]
-              {:metric 2 :a true}
-
-              [{:metric 10} {:metric -7} {:metric 2}]
-              {:metric -140}))
