39b246c28e4e9c877876ffd4998214c723c88687
Aphyr <aphyr@aphyr.com>
2013-03-09 18:57:34 -0800

    Client test: update for new reconnect strategy.

--- test/riemann/test/client.clj
+++ test/riemann/test/client.clj
@@ -14,18 +14,21 @@
                    (let [server (tcp-server)
                          core   (transition! (core) {:services [server]})
                          client (tcp-client)]
+                     (-> client .transport .transport .reconnectDelay (.set 0))
                      (try
                        ; Initial connection works
                        (is (send-event client {:service "test"}))
 
                        ; Kill server; should fail.
                        (stop! core)
-                       (.setMinimumReconnectInterval client 0)
-                       (is (thrown? java.net.SocketException
+                       (is (thrown? java.io.IOException
                                     (send-event client {:service "test"})))
+                       
 
                        ; Restart server; should work
                        (start! core)
+                       (Thread/sleep 200)
+
                        (try
                          (send-event client {:service "test"})
                          (finally
@@ -51,7 +54,6 @@
                (let [e (try (query client "invalid!")
                       (catch com.aphyr.riemann.client.ServerError e e))]
                  (is (= "parse error: invalid term \"invalid\"" (.getMessage e))))
-                 
                
                (finally
                  (close-client client)
