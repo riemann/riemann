706287c7022d24008b48be85f0865832b99e1973
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-04-04 11:05:24 +0200

    revert change to project.clj

--- project.clj
+++ project.clj
@@ -6,8 +6,7 @@
 ;  :jvm-opts ["-server" "-d64" "-Xms1024m" "-Xmx1024m" "-XX:+UseParNewGC" "-XX:+UseConcMarkSweepGC" "-XX:+CMSParallelRemarkEnabled" "-XX:+AggressiveOpts" "-XX:+UseFastAccessorMethods" "-verbose:gc" "-XX:+PrintGCDetails"]
   :jvm-opts ["-server" "-Xms1024m" "-Xmx1024m" "-XX:+UseParNewGC" "-XX:+UseConcMarkSweepGC" "-XX:+CMSParallelRemarkEnabled" "-XX:+AggressiveOpts" "-XX:+UseFastAccessorMethods"]
   :repositories {
-                 "boundary-site" "http://maven.boundary.com/artifactory/repo"
-                 "sonatype-snaps" "http://oss.sonatype.org/content/repositories/snapshots"
+    "boundary-site" "http://maven.boundary.com/artifactory/repo"
   }
   :maintainer {:email "aphyr@aphyr.com"}
   :dependencies [
