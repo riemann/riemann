235934dc3b7339b5beb010d37eca2ea6fcc1eb38
Gavin Sandie <g.sandie@digital-science.com>
2013-05-26 09:53:39 +0000

    Improve message formatting and fn descriptions

--- src/riemann/campfire.clj
+++ src/riemann/campfire.clj
@@ -1,6 +1,6 @@
 (ns riemann.campfire
   "Forwards events to Campfire"
-  (:use [clojure.string :only [join]])
+  (:use [clojure.string :only [join upper-case]])
   (:require [clj-campfire.core :as cf]))
 
 (defn cf-settings
@@ -15,15 +15,15 @@
   [settings room-name]
   (cf/room-by-name settings room-name))
 
-(defn format_string
+(defn campfire_message
+  "Formats an event into a string"
   [e]
-  (str (join " " ["HOST:" (str (:host e)) "SERVICE:" (str (:service e)) "STATE:" (str (:state e)) "DESC:" (str (:description e))]))
-  )
+  (str (join " " ["Riemann alert on" (str (:host e)) "-" (str (:service e)) "is" (upper-case (str (:state e))) "- Description:" (str (:description e))])))
 
 (defn campfire
-  "Creates an adaptor to forward events to campfire.
-  TODO: write more here once the event formatting is better.
-  TODO: tests
+  "Creates an adaptor to forward events to campfire. The campfire event will
+  contain the host, state, service, metric and description.
+
   Tested with:
   (streams
     (by [:host, :service]
@@ -31,7 +31,6 @@
         camp)))"
   [token ssl sub-domain room-name]
   (fn [e]
-    (let [string (format_string e)
-          settings (cf-settings token ssl sub-domain)
-          ]
+    (let [string (campfire_message e)
+          settings (cf-settings token ssl sub-domain)]
       (cf/message (room settings room-name) string))))
