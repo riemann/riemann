50033909308dc258b2914052a2c71a0ab5fa0f84
Pierre-Yves Ritschard <pyr@spootnik.org>
2013-10-28 22:12:51 +0100

    add graphite client and server test code

--- test/riemann/test/graphite.clj
+++ test/riemann/test/graphite.clj
@@ -2,9 +2,39 @@
   (:use riemann.graphite
         [riemann.time :only [unix-time]]
         clojure.test)
-  (:require [riemann.logging :as logging]))
+  (:require [riemann.logging :as logging]
+            [riemann.client :as client]
+            [riemann.index :as index]
+            [riemann.core :refer [transition! core stop!]]
+            [riemann.transport.tcp :refer [tcp-server]]
+            [riemann.transport.graphite :refer [graphite-server]]))
 
 (logging/init)
+
+(deftest graphite-server-test
+  (logging/suppress
+   ["riemann.transport" "riemann.core" "riemann.pubsub" "riemann.graphite"]
+   (let [s1       (graphite-server)
+         s2       (tcp-server)
+         index    (index/index)
+         core     (transition! 
+                   (core)
+                   {:index    index
+                    :services [s1 s2]
+                    :streams  [(partial index/update index)]})
+         sendout! (graphite {:path graphite-path-basic})
+         client   (client/tcp-client)]
+     (try
+       (sendout! {:service "service1" :metric 1.0 :time 0})
+       (sendout! {:service "service2" :metric 1.0 :time 0})
+       (Thread/sleep 100)
+       (let [[r1 r2] (client/query client "true")]
+         (is (and (#{"service1" "service2"} (:service r1))
+                  (= 1.0 (:metric r1))))
+         (is (and (#{"service1" "service2"} (:service r2))
+                  (= 1.0 (:metric r2)))))
+       (finally
+         (stop! core))))))
 
 (deftest percentiles
          (is (= (graphite-path-percentiles
