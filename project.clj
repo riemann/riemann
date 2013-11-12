74a756b3af39c98626fba23b75c6329edfc7cbd8
Kyle Kingsbury <aphyr@aphyr.com>
2013-11-11 17:28:52 -0800

    tools.logging 0.2.6

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
     [org.clojure/algo.generic "0.1.0"]
     [org.clojure/clojure "1.5.1"]
     [org.clojure/math.numeric-tower "0.0.1"]
-    [org.clojure/tools.logging "0.2.3"]
+    [org.clojure/tools.logging "0.2.6"]
     [org.clojure/tools.nrepl "0.2.3"]
     [org.clojure/java.classpath "0.2.0"]
     [clojure-complete "0.2.3"]
