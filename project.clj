feed82e5b303745fcf7de224511580e088bb6516
Kyle Kingsbury <aphyr@aphyr.com>
2013-11-11 17:06:41 -0800

    Netty 3.8.0.Final

--- project.clj
+++ project.clj
@@ -32,7 +32,7 @@
     [com.amazonaws/aws-java-sdk "1.4.1"]
     [incanter/incanter-core "1.4.1"]
     [interval-metrics "0.0.2"] 
-    [io.netty/netty "3.6.1.Final"]
+    [io.netty/netty "3.8.0.Final"]
     [log4j/apache-log4j-extras "1.0"]
     [org.antlr/antlr "3.2"]
     [org.slf4j/slf4j-log4j12 "1.6.4"]
