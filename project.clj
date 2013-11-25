c03411cf8e71e6e7c1e4dc79c07fa1f7d13c9719
Kyle Kingsbury <aphyr@aphyr.com>
2013-11-25 15:47:20 -0800

    0.2.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riemann "0.2.4-SNAPSHOT"
+(defproject riemann "0.2.4"
   :description
 "A network event stream processor. Intended for analytics, metrics, and alerting; and to glue various monitoring systems together."
   :url "http://github.com/aphyr/riemann"
