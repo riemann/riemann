48ff6fdb57e591b408ab2bf6b02b18b7f97b1d2b
Gavin Sandie <beach@vicecity.co.uk>
2014-04-06 20:44:15 +0100

    refactor slack notifications and make them work

--- src/riemann/slack.clj
+++ src/riemann/slack.clj
@@ -8,32 +8,25 @@
     "Format message according to slack formatting spec."
     (escape message {\< "&lt;" \> "&gt;"}))
 
-  ;  (client/post hook_url {:form-params
-  ;     {:payload (json/write-str {:text "OMG" :channel "#alerts" :username "alerter" :icon_emoji ":warning:"})}})
-
-
-  (defn slack_message
-    ""
-    [username channel]
-    (fn [event]
-      (json/generate-string
-        (slack_format
-          {:text (str event)
-           :channel channel
-           :username username
-           :icon_emoji ":warning:"}))))
-
-  (defn slack_url
-    ""
-    [ac_name token]
-    (str "https://" ac_name ".slack.com/services/hooks/incoming-webhook?token=" token))
-
-
   (defn slack
-    ""
-    [account_name token username channel]
-    (prn (slack_message username channel)))
-  (client/post (slack_url account_name token)
-               {:form-params {:payload (str (slack_message username channel ))}}))
+    "Posts events into a slack.com channel using Incoming Webhooks.
+    Takes your account name, webhook token, bot username and room name.
+    Returns a function that will post a message into slack.com room.
 
-
+    (by [:service]
+      (let [alert (slack \"some_org\" \"....\" \"Riemann bot\" \"#monitoring\")]
+                                                      alert))
+    "
+    [account_name token username room]
+    (fn [event]
+      (client/post (str "https://" account_name ".slack.com/services/hooks/incoming-webhook?token=" token)
+                   {:form-params
+                    {:payload (json/generate-string
+                                {:text (slack_format (str "*Host:* " (:host event)
+                                                          " *State:* " (:state event)
+                                                          " *Description:* " (:description event)
+                                                          " *Metric:* " (:metric event)
+                                                          ))
+                                 :channel room
+                                 :username username
+                                 :icon_emoji ":warning:"})}})))
