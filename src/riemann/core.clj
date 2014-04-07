d85ff5c945888f936ff369db4a057460a6d0f65f
Aphyr <aphyr@aphyr.com>
2014-04-07 10:23:11 -0700

    Drop warning from core index wrapper for events without time

--- src/riemann/core.clj
+++ src/riemann/core.clj
@@ -204,8 +204,7 @@
          (index/search source query-ast))
        (update [this event]
          (when-not (:time event)
-           (warn "trying to index event with no time")
-           (throw (ex-info "cannot index events with no time"
+           (throw (ex-info "cannot index event with no time"
                            {:event event})))
          (index/update source event)
          (when registry
