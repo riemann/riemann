4a6edc8fd09d71838935b8e3750ebf38c438ece3
Aphyr <aphyr@aphyr.com>
2014-04-14 10:46:53 -0700

    Clarify graphite-server docstring

--- src/riemann/transport/graphite.clj
+++ src/riemann/transport/graphite.clj
@@ -99,7 +99,7 @@
 
   :host       \"127.0.0.1\"
   :port       2003
-  :protocol   tcp or udp (tcp is default)
+  :protocol   :tcp or :udp (default :tcp)
   :parser-fn  an optional function given to decode-graphite-line"
   ([] (graphite-server {}))
   ([opts]
