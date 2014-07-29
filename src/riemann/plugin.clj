fa56165756fe926c832351ef5ee31ed883ec0e4f
Pierre-Yves Ritschard <pyr@spootnik.org>
2014-07-29 23:03:06 +0200

    Allow dynamically loaded namespace and provide (depend)

--- src/riemann/plugin.clj
+++ src/riemann/plugin.clj
@@ -23,6 +23,10 @@
             [clojure.java.io        :refer [resource]]
             [clojure.tools.logging  :refer [info]]))
 
+(def repo
+  "Default location of local maven repository"
+  (atom "/etc/riemann/plugins"))
+
 (defn read-safely
   "Read data without evaluation"
   [src]
@@ -31,33 +35,44 @@
 
 (defn load-from-meta
   "Given a metadata description map, require plugin's namespace"
-  [{:keys [plugin require]}]
+  [{:keys [plugin require]} {:keys [alias]}]
   (let [to-symbol (comp symbol name)]
     (when require
       (info "loading plugin:" plugin)
-      (clojure.core/require [(to-symbol require) :as (to-symbol plugin)]))))
+      (if alias
+        (clojure.core/require [(to-symbol require) :as (to-symbol plugin)])
+        (clojure.core/require [(to-symbol require)])))))
 
 (defn load-from-resource
   "Given a path to a java resource, load metadata and require plugin"
-  [src]
+  [src options]
   (-> src
       resource
       slurp
       read-safely
-      load-from-meta))
+      (load-from-meta options)))
 
 (defn load-plugins
-  "Walk classpath and try to load all plugins that were found."
-  []
-  (info "walking classpath to find plugins")
-  (let [plugin-desc? (partial re-matches #"riemann_plugin/(.*)/meta.edn$")
-        files        (mapcat cp/filenames-in-jar (cp/classpath-jarfiles))]
-    (doseq [desc-file (filter plugin-desc? files)]
-      (load-from-resource desc-file))))
+  "Walk classpath and try to load all plugins that were found.
+   Optionally accepts an option map containing the :alias keyword which determines
+   whether to create named aliases for loaded plugins"
+  ([options]
+     (info "walking classpath to find plugins")
+     (let [plugin-desc? (partial re-matches #"riemann_plugin/(.*)/meta.edn$")
+           files        (mapcat cp/filenames-in-jar (cp/classpath-jarfiles))]
+       (doseq [desc-file (filter plugin-desc? files)]
+         (load-from-resource desc-file options))))
+  ([]
+     (load-plugins {:alias true})))
 
 (defn load-plugin
   "Given a plugin name, look for its metadata description file, and
-   require plugin's namespace"
-  [plugin]
-  (load-from-resource (format "riemann_plugin/%s/meta.edn" (name plugin))))
-
+   require plugin's namespace.
+   Optionally accepts an option map containing the :alias keyword which determines
+   whether to create named aliases for the loaded plugin"
+  ([plugin]
+     (load-from-resource (format "riemann_plugin/%s/meta.edn" (name plugin))
+                         {:alias true}))
+  ([plugin options]
+     (load-from-resource (format "riemann_plugin/%s/meta.edn" (name plugin))
+                         options)))
