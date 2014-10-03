9ad438976c6cfd65477920503479508fd2cb76ea
Gleb Pomykalov <gleb@pomykalov.ru>
2014-10-03 14:40:17 +0400

    Opsgenie tests

---
+++ test/riemann/opsgenie_test.clj
@@ -0,0 +1,31 @@
+(ns riemann.opsgenie-test
+  (:use riemann.opsgenie
+        clojure.test)
+  (:require [riemann.logging :as logging]))
+
+(def service-key (System/getenv "OPSGENIE_SERVICE_KEY"))
+(def recipients (System/getenv "OPSGENIE_RECIPIENTS"))
+
+(when-not service-key
+  (println "export OPSGENIE_SERVICE_KEY=\"...\" to run these tests."))
+
+(when-not recipients
+  (println "export OPSGENIE_RECIPIENTS=\"...\" to run these tests."))
+
+(logging/init)
+
+(deftest ^:opsgenie ^:integration test-resolve
+  (let [og (opsgenie service-key recipients)]
+    ((:resolve og) {:host "localhost"
+                  :service "opsgenie notification"
+                  :description "Testing resolving event"
+                  :metric 42
+                  :state "ok"})))
+
+(deftest ^:opsgenie ^:integration test-trigger
+  (let [og (opsgenie service-key recipients)]
+    ((:trigger og) {:host "localhost"
+                  :service "opsgenie notification"
+                  :description "Testing triggering event"
+                  :metric 20
+                  :state "error"})))
