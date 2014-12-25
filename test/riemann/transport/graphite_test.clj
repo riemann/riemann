d59f1f8886610b1d553cbfb7af6238057fb4ca39
Aphyr <aphyr@aphyr.com>
2014-12-25 01:23:38 -0800

    Graphite server roundtrip test

--- test/riemann/transport/graphite_test.clj
+++ test/riemann/transport/graphite_test.clj
@@ -3,7 +3,9 @@
         [riemann.common :only [event]]
         riemann.transport.graphite
         [slingshot.slingshot :only [try+]])
-  (:require [riemann.logging :as logging]))
+  (:require [riemann.logging :as logging]
+            [riemann.core :as core]
+            [riemann.graphite :as client]))
 
 (deftest decode-graphite-line-success-test
   (is (= (event {:service "name", :metric 123.0, :time 456})
@@ -34,3 +36,32 @@
     (is (= (err "name\twith\ttab\t123\t456") "too many fields"))
     (is (= (err "name with space\tand\ttab 123\t456") "too many fields"))
     (is (= (err "name\t\t123\t456\t\t\t789") "too many fields"))))
+
+(deftest round-trip-test
+  (riemann.logging/suppress ["riemann.transport"
+                             "riemann.pubsub"
+                             "riemann.graphite"
+                             "riemann.core"]
+    (let [server (graphite-server)
+          sink   (promise)
+          core   (core/transition! (core/core)
+                                   {:services [server]
+                                    :streams  [(partial deliver sink)]})]
+      (try
+        ; Open a client and send an event
+        (let [client (client/graphite {:pool-size 1 :block-start true})]
+          (client {:host "computar"
+                   :service "hi there" :metric 2.5 :time 123 :ttl 10})
+
+          ; Verify event arrives
+          (is (= (deref sink 1000 :timed-out)
+                 (event {:host        nil
+                         :service     "computar.hi.there"
+                         :state       nil
+                         :description nil
+                         :metric      2.5
+                         :tags        nil
+                         :time        123
+                         :ttl         nil}))))
+        (finally
+          (core/stop! core))))))
