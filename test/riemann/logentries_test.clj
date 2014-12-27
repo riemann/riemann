9569bd6a65470286505a84e1ce9b885ae4f15469
Kyle Kingsbury <aphyr@aphyr.com>
2014-12-26 21:03:03 -0800

    logentries-test: suppress logging.

--- test/riemann/logentries_test.clj
+++ test/riemann/logentries_test.clj
@@ -57,19 +57,20 @@
            "New user, service='production front' state='ok'"))))
 
 (deftest test-logentries
-  (let [le-stub (open (DefaultLogentriesStub. port))
-        le (logentries {:host host
-                        :port port
-                        :token token
-                        :pool-size 1
-                        :claim-timeout 1})]
-    (le (test-event {:description "Test event 1"}))
-    (le (test-event {:description "Test event 2"}))
-    (let [in @(:input le-stub)
-          line-1 (.readLine in)
-          line-2 (.readLine in)]
-      (is (.startsWith line-1 "Test event 1"))
-      (is (.startsWith line-2 "Test event 2"))
-      (is (.endsWith line-1 token))
-      (is (.endsWith line-2 token)))
-    (close le-stub)))
+  (logging/suppress ["riemann.logentries"]
+    (let [le-stub (open (DefaultLogentriesStub. port))
+          le (logentries {:host host
+                          :port port
+                          :token token
+                          :pool-size 1
+                          :claim-timeout 1})]
+      (le (test-event {:description "Test event 1"}))
+      (le (test-event {:description "Test event 2"}))
+      (let [in @(:input le-stub)
+            line-1 (.readLine in)
+            line-2 (.readLine in)]
+        (is (.startsWith line-1 "Test event 1"))
+        (is (.startsWith line-2 "Test event 2"))
+        (is (.endsWith line-1 token))
+        (is (.endsWith line-2 token)))
+      (close le-stub))))
