ca18e995ee562cd47371b0a0934bbf4db28ddcca
Kyle Kingsbury <aphyr@aphyr.com>
2014-12-26 20:54:38 -0800

    shinken-test: remove HTTP server and replace with mocked clj-http/client

--- test/riemann/shinken_test.clj
+++ test/riemann/shinken_test.clj
@@ -1,69 +1,63 @@
 (ns riemann.shinken-test
-  (:use [riemann.time :only [unix-time]])
-  (:use riemann.shinken
-        aleph.http
+  (:use [riemann.time :only [unix-time]]
+        riemann.shinken
         clojure.test)
-  (:require [riemann.logging :as logging]))
+  (:require [clj-http.client :as http]
+            [riemann.test-utils :refer [with-mock]]
+            [riemann.logging :as logging]))
 
 (logging/init)
 
 (deftest ^:shinken shinken-test
-  (let [shkn (shinken {})
-        time (unix-time)]
+  (with-mock [calls clj-http.client/post]
+    (let [shkn (shinken {})
+          time (unix-time)]
 
-    "Test an event with unix time"
-    (let [body (promise)
-          e {:host "testhost"
-             :service "testservice"
-             :description "testdsecription"
-             :metric 42
-             :time (/ 351406934039 250)
-             :state "ok"}
-          expected_body "time_stamp=1405627736&host_name=testhost&service_description=testservice&return_code=ok&output=42"
-          stop-server (start-http-server
-                        (wrap-ring-handler
-                          (fn [req]
-                            (deliver body (slurp (:body req)))
-                            {:status 200}))
-                        {:port 7760})]
-      (shkn e)
-      (is (= expected_body @body))
-      (stop-server))
+      (testing "an event with unix time"
+        (shkn {:host "testhost"
+                 :service "testservice"
+                 :description "testdsecription"
+                 :metric 42
+                 :time (/ 351406934039 250)
+                 :state "ok"})
+        (is (= (last @calls)
+               ["http://127.0.0.1:7760/push_check_result"
+                {:basic-auth ["admin" "admin"]
+                 :form-params {:time_stamp 1405627736,
+                               :host_name "testhost"
+                               :service_description "testservice"
+                               :return_code "ok"
+                               :output 42}}])))
 
-    "Test sending to another port"
-    (let [body (promise)
-          e {:host "testhost"
-             :service "testservice"
-             :description "testdsecription"
-             :metric 42
-             :time 1405458798
-             :state "ok"}
-          expected_body "time_stamp=1405458798&host_name=testhost&service_description=testservice&return_code=ok&output=42"
-          stop-server (start-http-server
-                        (wrap-ring-handler
-                          (fn [req]
-                            (deliver body (slurp (:body req)))
-                            {:status 200}))
-                        {:port 7761})]
-      ((shinken {:port 7761}) e)
-      (is (= expected_body @body))
-      (stop-server))
+      (testing "sending to another port"
+        ((shinken {:host "verne" :port 7761})
+         {:host "testhost"
+          :service "testservice"
+          :description "testdsecription"
+          :metric 42
+          :time 1405458798
+          :state "ok"})
+         (is (= (last @calls)
+                ["http://127.0.0.1:7761/push_check_result"
+                 {:basic-auth ["admin" "admin"]
+                  :form-params {:time_stamp 1405458798
+                                :host_name "testhost"
+                                :service_description "testservice"
+                                :return_code "ok"
+                                :output 42}}])))
 
-    "Test with a string as a metric and an integer as the state"
-    (let [body (promise)
-          e {:host "testhost"
-             :service "testservice"
-             :description "testdsecription"
-             :metric "stringmetric"
-             :time 1405458798
-             :state 0}
-          expected_body "time_stamp=1405458798&host_name=testhost&service_description=testservice&return_code=0&output=stringmetric"
-          stop-server (start-http-server
-                        (wrap-ring-handler
-                          (fn [req]
-                            (deliver body (slurp (:body req)))
-                            {:status 200}))
-                        {:port 7760})]
-      (shkn e)
-      (is (= expected_body @body))
-      (stop-server))))
+      (testing "a string as a metric and an integer as the state"
+        (shkn {:host "testhost"
+               :service "testservice"
+               :description "testdsecription"
+               :metric "stringmetric"
+               :time 1405458798
+               :state 0})
+        (is (= (last @calls)
+               ["http://127.0.0.1:7760/push_check_result"
+                {:basic-auth ["admin" "admin"]
+                 :form-params {:time_stamp 1405458798
+                               :host_name "testhost"
+                               :service_description "testservice"
+                               :return_code 0
+                               :output "stringmetric"}}]))))))
