7a41583601e1b368c912cf3cf3b8d854be1f520d
Aphyr <aphyr@aphyr.com>
2015-01-05 11:53:31 -0800

    client-0.3.0wq

--- project.clj
+++ project.clj
@@ -37,7 +37,7 @@
     [log4j/apache-log4j-extras "1.0"]
     [org.antlr/antlr "3.2"]
     [org.slf4j/slf4j-log4j12 "1.7.7"]
-    [riemann-clojure-client "0.2.10"]
+    [riemann-clojure-client "0.3.0-SNAPSHOT"]
     [slingshot "0.10.3"]
     [clj-campfire "2.2.0"]
     [less-awful-ssl "0.1.1"]
