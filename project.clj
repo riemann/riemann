45d4abc6a2b6293f19e7ec7f7ed9a28b8772c7e6
Kyle Kingsbury <aphyr@aphyr.com>
2015-01-05 12:48:04 -0800

    Comment out JVM profiling opts; jdk7 doesn't know bout em.

--- project.clj
+++ project.clj
@@ -47,10 +47,10 @@
      :exclusions [http-kit]]]
   :plugins [[codox "0.6.1"]
             [lein-rpm "0.0.5"]]
-  :profiles {:dev {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"
-                              "-Dcom.sun.management.jmxremote"
-                              "-XX:+UnlockCommercialFeatures"
-                              "-XX:+FlightRecorder"]
+  :profiles {:dev {:jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
+;                              "-Dcom.sun.management.jmxremote"
+;                              "-XX:+UnlockCommercialFeatures"
+;                              "-XX:+FlightRecorder"]
                    :dependencies [[criterium "0.4.3"]]}}
   :test-selectors {:default (fn [x] (not (or (:integration x)
                                              (:time x)
