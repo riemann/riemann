886a07b329bb62151640611a18178d66dc045746
Aphyr <aphyr@aphyr.com>
2015-01-21 23:29:49 -0800

    tools.nrepl 0.2.3 -> 0.2.7

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
     [org.clojure/clojure "1.6.0"]
     [org.clojure/math.numeric-tower "0.0.4"]
     [org.clojure/tools.logging "0.3.1"]
-    [org.clojure/tools.nrepl "0.2.3"]
+    [org.clojure/tools.nrepl "0.2.7"]
     [org.clojure/core.cache "0.6.4"]
     [org.clojure/java.classpath "0.2.2"]
     [log4j/log4j "1.2.17" :exclusions [javax.mail/mail
