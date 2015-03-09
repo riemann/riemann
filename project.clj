0d8adc0cc4079e4dd42b9033bb1a7c7fd7b070ec
Aphyr <aphyr@aphyr.com>
2015-03-08 23:16:28 -0700

    0.2.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riemann "0.2.9-SNAPSHOT"
+(defproject riemann "0.2.9"
   :description
 "A network event stream processor. Intended for analytics, metrics, and alerting; and to glue various monitoring systems together."
   :url "http://github.com/aphyr/riemann"
