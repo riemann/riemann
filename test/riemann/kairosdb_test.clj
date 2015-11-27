2ed66c69505d6523141ac875c5edf4c2d4108471
Ryan Crum <rcrum@walmartlabs.com>
2015-11-27 12:40:29 -0500

    Fix up indentation in kairosdb test

--- test/riemann/kairosdb_test.clj
+++ test/riemann/kairosdb_test.clj
@@ -7,24 +7,25 @@
 (logging/init)
 
 (deftest ^:kairosdb ^:integration kairosdb-test
-         (let [k (kairosdb {:block-start true})]
-           (k {:host "riemann.local"
-               :service "kairosdb test"
-               :state "ok"
-               :description "all clear, uh, situation normal"
-               :metric -2
-               :time (unix-time)}))
+  (let [k (kairosdb {:block-start true})]
+    (k {:host "riemann.local"
+        :service "kairosdb test"
+        :state "ok"
+        :description "all clear, uh, situation normal"
+        :metric -2
+        :time (unix-time)}))
 
-         (let [k (kairosdb {:block-start true})]
-           (k {:service "kairosdb test"
-               :state "ok"
-               :description "all clear, uh, situation normal"
-               :metric 3.14159
-               :time (unix-time)}))
+  (let [k (kairosdb {:block-start true})]
+    (k {:service "kairosdb test"
+        :state "ok"
+        :description "all clear, uh, situation normal"
+        :metric 3.14159
+        :time (unix-time)}))
 
-         (let [k (kairosdb {:block-start true})]
-           (k {:host "no-service.riemann.local"
-               :state "ok"
-               :description "Missing service, not transmitted"
-               :metric 4
-               :time (unix-time)})))
+  (let [k (kairosdb {:block-start true})]
+    (k {:host "no-service.riemann.local"
+        :state "ok"
+        :description "Missing service, not transmitted"
+        :metric 4
+        :time (unix-time)})))
+
