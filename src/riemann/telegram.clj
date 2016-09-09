e8038789f8005c5cd1781945314281b2ba2f142e
Pradeep Chhetri <pradeep.chhetri89@gmail.com>
2016-09-10 00:44:58 +0530

    Minor cleanup

--- src/riemann/telegram.clj
+++ src/riemann/telegram.clj
@@ -17,17 +17,17 @@
               "<strong>State:</strong> " (or (:state e) "-") "\n"
               "<strong>Metric:</strong> " (or (:metric e) "-") "\n"
               "<strong>Description:</strong> "
-                (escape (or (:description e) "-") {\< "&lt;", \> "&gt;", \& "&amp;"}))) 
+                (escape (or (:description e) "-") {\< "&lt;", \> "&gt;", \& "&amp;"})))
           (flatten [ev]))))
 
 (defn- post
   "POST to the Telegram API."
   [token chat_id event]
   (client/post (format api-url token "sendMessage")
-               {:form-params {:chat_id chat_id 
+               {:form-params {:chat_id chat_id
                               :parse_mode "HTML"
                               :text (format-message event)}
-		:throw-entire-message? true}))
+                :throw-entire-message? true}))
 
 (defn telegram
   "Send events to Telegram chat. Uses your bot token and returns a function,
@@ -43,12 +43,23 @@
   (def chat_id \"0123456\")
 
   (streams
-    (rollup 5 3600 (telegram {:token token :chat_id chat_id})))"
-  [{:keys [token chat_id]}]
-  (fn [e]
-    (let [events (if (sequential? e)
-		   e
-		   [e])]
-      (doseq [event events]
-	(post token chat_id event)
-	(Thread/sleep 1000)))))
+    (rollup 5 3600 (telegram {:token token :chat_id chat_id})))
+
+  Example:
+
+  (def telegram-async
+    (batch 10 1
+      (async-queue!
+        :telegram-async                         ; A name for the forwarder
+          {:queue-size     1e4                  ; 10,000 events max
+           :core-pool-size 5                    ; Minimum 5 threads
+           :max-pools-size 100}                 ; Maximum 100 threads
+          (telegram {:token \"275347130:AAEdWBudgeQCV87O0ag9luwwFGcN2Efeqk4\"
+                     :chat_id \"261032559\" }))))
+  "
+  [opts]
+  (fn [event]
+    (let [events (if (sequential? event)
+                   event
+                   [event])]
+      (post (:token opts) (:chat_id opts) event))))
