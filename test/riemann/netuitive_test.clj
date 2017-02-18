81b7dcb36b67ed6b7c3f79189a8724daa4210859
Mathieu Corbin <corbin.math@gmail.com>
2017-02-18 20:48:36 +0100

    Remove trailing whitespaces on the netuitive code (#779)

--- test/riemann/netuitive_test.clj
+++ test/riemann/netuitive_test.clj
@@ -2,10 +2,9 @@
   (:use riemann.netuitive
         clojure.test))
 
-(def test-event {:host "riemann.local" :service "netuitive test" :state "ok" :description "Successful test" :metric 2 :time (/ (System/currentTimeMillis) 1000) :tags ["riemann" "netuitive"]})	
-   
+(def test-event {:host "riemann.local" :service "netuitive test" :state "ok" :description "Successful test" :metric 2 :time (/ (System/currentTimeMillis) 1000) :tags ["riemann" "netuitive"]})
+
 (deftest ^:netuitive netuitive-unit-tests
-		   
    (is (= (:type (generate-event test-event {})) "Riemann"))
    (is (= (:id (generate-event test-event {})) "Riemann:riemann.local"))
    (is (= (:name (generate-event test-event {})) "riemann.local"))
@@ -13,19 +12,13 @@
    (is (= (:metricId (first (:samples (generate-event test-event {}))) "netuitive.test")))
    (is (= (:val (first (:samples (generate-event test-event {}))) 2)))
    (is (= (:name (second (:tags (generate-event test-event {})))) "netuitive"))
-   
    (is (= (:name (generate-tag "tagname") "tagname")))
-   
    (is (= (:name (first (:tags (generate-event test-event {}))) "riemann")))
-   
    (is (= (:type (generate-event test-event {:type "SERVER"})) "SERVER"))
-   
    (is (= (netuitive-metric-name test-event) "netuitive.test")))
 
 (deftest ^:integration ^:netuitive netuitive-test
-
    (let [k (netuitive {:api-key "netuitive-test-key" :url "https://api.app.netuitive.com/ingest/"})]
      (k test-event))
-		   
    (let [k (netuitive {:api-key "netuitive-test-key"})]
      (k test-event)))
