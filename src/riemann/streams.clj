04708e53b5f23887111256e5e684f3d610019920
mcorbin <corbin.math@gmail.com>
2017-03-20 08:24:09 +0100

    Add cond-dt, above, below, between, outside streams
    These streams can be used to detect if all events during a time period
    validate a condition. If it is, they passe on the last event
    validation the condition.

--- src/riemann/streams.clj
+++ src/riemann/streams.clj
@@ -2089,3 +2089,72 @@
                             (dissoc ::clock-skew-timestamp)
                             (assoc :metric delta))]
                 (call-rescue event children)))))))
+
+(defn cond-dt
+  "A stream which detects if a condition `(f event)` is true during `dt` seconds.
+  Takes a function of events `f` and a time period `dt` in seconds.
+&
+    If the condition is valid for all events received during at least the period `dt`, valid events received after the `dt` period will be passed on until an invalid event arrives.
+  Skips events that are too old or that do not have a time stamp."
+  [f dt & children]
+  (let [last-changed-state (atom {:ok false
+                                  :time nil})]
+    (fn [event]
+      (let [{ok :ok changed-state-time :time} @last-changed-state
+            event-time (:time event)
+            valid-event (f event)]
+        (when event-time ;; filter events with no time
+          (swap! last-changed-state (fn [state]
+                        (cond
+                          (and valid-event (and (not ok)
+                                                (or (not changed-state-time)
+                                                    (> event-time changed-state-time))))
+                          ;; event is validating the condition
+                          ;; last event is not ok, has no time or is too old
+                          ;; => last-changed-state is now ok with a new time
+                          {:ok true :time event-time}
+                          (and (not valid-event) (and ok
+                                                      (or (not changed-state-time)
+                                                          (> event-time changed-state-time))))
+                          ;; event is not validating the condition
+                          ;; last event is ok, has no time or is too old
+                          ;; => last-changed-state is now ko with a new time
+                          {:ok false :time event-time}
+                          ;; default value, return the state
+                          :default state)))
+          (when (and valid-event
+                      ;; we already had an ok event
+                     ok
+                     ;; check is current time > first ok event + dt
+                     (> event-time (+ changed-state-time dt)))
+            (call-rescue event children)))))))
+
+(defn above
+  "Takes a number `threshold` and a time period in seconds `dt`.
+  If the condition `(> (:metric event) threshold)` is valid for all events received during at least the period `dt`, valid events received after the `dt` period will be passed on until an invalid event arrives.
+  `:metric` should not be nil (it will produce exceptions)."
+  [threshold dt & children]
+  (apply cond-dt (fn [event] (> (:metric event) threshold)) dt children))
+
+(defn below
+  "Takes a number `threshold` and a time period in seconds `dt`.
+  If the condition `(< (:metric event) threshold)` is valid for all events received during at least the period `dt`, valid events received after the `dt` period will be passed on until an invalid event arrives.
+  `:metric` should not be nil (it will produce exceptions)."
+  [threshold dt & children]
+  (apply cond-dt (fn [event] (< (:metric event) threshold)) dt children))
+
+(defn between
+  "Takes two numbers, `low` and `high`, and a time period in seconds, `dt`.
+  If the condition `(and (> (:metric event) low) (< (:metric event) high))` is valid for all events received during at least the period `dt`, valid events received after the `dt` period will be passed on until an invalid event arrives.
+  `:metric` should not be nil (it will produce exceptions)."
+  [low high dt & children]
+  (apply cond-dt (fn [event] (and (> (:metric event) low)
+                                  (< (:metric event) high))) dt children))
+
+(defn outside
+  "Takes two numbers, `low` and `high`, and a time period in seconds, `dt`.
+  If the condition `(or (< (:metric event) low) (> (:metric event) high))` is valid for all events received during at least the period `dt`, valid events received after the `dt` period will be passed on until an invalid event arrives.
+  `:metric` should not be nil (it will produce exceptions)."
+  [low high dt & children]
+  (apply cond-dt (fn [event] (or (< (:metric event) low)
+                                 (> (:metric event) high))) dt children))
--- test/riemann/streams_test.clj
+++ test/riemann/streams_test.clj
@@ -1691,3 +1691,248 @@
                                  {:time 9 :host :baz :metric 5}
                                  {:time 1 :host :foo :metric -4}
                                  {:time 100 :host :foo :metric 89}]))
+
+(deftest above-test
+  (testing "do nothing"
+    (test-stream (above 10 5) [] [])
+
+    (test-stream (above 10 5) [{:time 0
+                                :metric 9}
+                               {:time 5
+                                :metric 11}
+                               {:time 6
+                                :metric 11}] [])
+
+    (test-stream (above 10 5) [{:time 0
+                                :metric 11}
+                               {:time 5
+                                :metric 9}
+                               {:time 11
+                                :metric 12}] [])
+
+    (test-stream (above 10 5) [{:time 0
+                                :metric 9}
+                               {:metric 11}
+                               {:time 6
+                                :metric 11}] [])
+
+    (test-stream (above 10 5) [{:time 0
+                                :metric 12}
+                               {:time 5
+                                :metric 12}
+                               {:time 4
+                                :metric 9}
+                               {:time 8
+                                :metric 11}] [])
+
+    (test-stream (above 10 5) [{:time 0
+                                :metric 12}
+                               {:metric 12}
+                               {:time 5
+                                :metric 11}
+                               {:time 4 ;; too old
+                                :metric 12}
+                               {:time 6
+                                :metric 9}
+                               {:time 16
+                                :metric 12}
+                               {:time 10
+                                :metric 11}] []))
+
+  (testing "fire alert"
+    (test-stream (above 10 5) [{:time 0
+                                :metric 11}
+                               {:time 6
+                                :metric 11}] [{:time 6
+                                               :metric 11}])
+    (test-stream (above 10 5) [{:time 0
+                                :metric 2}
+                               {:time 6
+                                :metric 11}
+                               {:time 12
+                                :metric 11}] [{:time 12
+                                               :metric 11}])
+    (test-stream (above 10 5) [{:time 0
+                                :metric 11}
+                               {:time 7
+                                :metric 4}
+                               {:time 9
+                                :metric 11}
+                               {:time 15
+                                :metric 12}] [{:time 15
+                                               :metric 12}])
+    (test-stream (above 10 5) [{:time 0
+                                :metric 11}
+                               {:time 7
+                                :metric 4}
+                               {:time 9 ;; event ok
+                                :metric 11}
+                               {:time 15 ;; fire event
+                                :metric 12}
+                               {:time 20 ;; fire event
+                                :metric 12}
+                               {:time 20 ;; fire event
+                                :metric 12}
+                               {:time 8 ;; too old
+                                :metric 9}
+                               {:time 10 ;; old but reset current-time to 10
+                                :metric 9}
+                               {:time 11 ;; event ok
+                                :metric 12}
+                               {:time 17 ;; fire event
+                                :metric 12}] [{:time 15
+                                               :metric 12}
+                                              {:time 20
+                                               :metric 12}
+                                              {:time 20
+                                               :metric 12}
+                                              {:time 17
+                                               :metric 12}])
+    (test-stream (above 10 5) [{:time 0
+                                :metric 11}
+                               {:time 7
+                                :metric 4}
+                               ;; ignore old ok event
+                               {:time 1
+                                :metric 12}
+                               ;; ignore old ok event
+                               {:time 6
+                                :metric 12}
+                               {:time 9
+                                :metric 11}
+                               {:time 15
+                                :metric 12}] [{:time 15
+                                               :metric 12}])
+    (test-stream (above 10 5) [{:time 0
+                                :metric 11}
+                               {:time 7
+                                :metric 4}
+                               ;; ignored
+                               {:time 6
+                                :metric 8}
+                               {:time 7.1
+                                :metric 11}
+                               {:time 15
+                                :metric 12}] [{:time 15
+                                               :metric 12}])))
+
+(deftest below-test
+  (testing "do nothing"
+    (test-stream (below 10 5) [] [])
+
+    (test-stream (below 10 5) [{:time 0
+                                :metric 11}
+                               {:time 5
+                                :metric 9}
+                               {:time 6
+                                :metric 9}] [])
+
+    (test-stream (below 10 5) [{:time 0
+                                :metric 9}
+                               {:time 5
+                                :metric 11}
+                               {:time 11
+                                :metric 7}] [])
+
+    (test-stream (below 10 5) [{:time 0
+                                :metric 11}
+                               {:metric 9}
+                               {:time 6
+                                :metric 9}] []))
+  (testing "fire alert"
+    (test-stream (below 10 5) [{:time 0
+                                :metric 9}
+                               {:time 6
+                                :metric 9}] [{:time 6
+                                              :metric 9}])
+    (test-stream (below 10 5) [{:time 0
+                                :metric 9}
+                               {:time 7
+                                :metric 15}
+                               {:time 9
+                                :metric 9}
+                               {:time 15
+                                :metric 7}] [{:time 15
+                                              :metric 7}])))
+
+(deftest between-test
+  (testing "do nothing"
+    (test-stream (between 10 20 5) [] [])
+
+    (test-stream (between 10 20 5) [{:time 0
+                                     :metric 9}
+                                    {:time 5
+                                     :metric 11}
+                                    {:time 6
+                                     :metric 11}] [])
+
+    (test-stream (between 10 20 5) [{:time 0
+                                     :metric 11}
+                                    {:time 1
+                                     :metric 11}
+                                    {:time 5
+                                     :metric 21}
+                                    {:time 11
+                                     :metric 12}] [])
+
+    (test-stream (between 10 20 5) [{:time 0
+                                     :metric 9}
+                                    {:metric 15}
+                                    {:time 6
+                                     :metric 15}] []))
+  (testing "fire alert"
+    (test-stream (between 10 20 5) [{:time 0
+                                     :metric 15}
+                                    {:time 6
+                                     :metric 19}] [{:time 6
+                                                    :metric 19}])
+    (test-stream (between 10 20 5) [{:time 0
+                                     :metric 11}
+                                    {:time 7
+                                     :metric 30}
+                                    {:time 9
+                                     :metric 15}
+                                    {:time 15
+                                     :metric 12}] [{:time 15
+                                                    :metric 12}])))
+
+(deftest outside-test
+  (testing "do nothing"
+    (test-stream (outside 10 20 5) [] [])
+
+    (test-stream (outside 10 20 5) [{:time 0
+                                     :metric 10}
+                                    {:time 5
+                                     :metric 20}
+                                    {:time 6
+                                     :metric 15}] [])
+
+    (test-stream (outside 10 20 5) [{:time 0
+                                     :metric 11}
+                                    {:time 1
+                                     :metric 11}
+                                    {:time 5
+                                     :metric 21}
+                                    {:time 11
+                                     :metric 12}] [])
+
+    (test-stream (outside 10 20 5) [{:time 0
+                                     :metric 9}
+                                    {:metric 15}
+                                    {:time 6
+                                     :metric 15}] []))
+  (testing "fire alert"
+    (test-stream (outside 10 20 5) [{:time 0
+                                     :metric 9}
+                                    {:time 6
+                                     :metric 21}] [{:time 6
+                                                    :metric 21}])
+    (test-stream (outside 10 20 5) [{:time 0
+                                     :metric 9}
+                                    {:time 7
+                                     :metric 16}
+                                    {:time 9
+                                     :metric 1}
+                                    {:time 15
+                                     :metric 2}] [{:time 15
+                                                   :metric 2}])))
