a9a363f4aa5ba46e3d53a4b944c1847027609d70
James Turnbull <james@lovedthanlost.net>
2017-04-06 00:38:56 -0400

    Added 0.2.14 SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject riemann "0.2.13"
+(defproject riemann "0.2.14-SNAPSHOT"
   :description
 "A network event stream processor. Intended for analytics, metrics, and alerting; and to glue various monitoring systems together."
   :url "http://github.com/riemann/riemann"
