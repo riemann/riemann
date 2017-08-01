27957ba33ceb175b7937a259de9087b8cf0cbb72
mcorbin <corbin.math@gmail.com>
2017-08-01 23:17:00 +0200

    Fix by-builder stream
    cf #699 : by-builder accepts multiple "forms" but passes events only
    to the last form.
    Also add tests for by-builder

--- src/riemann/streams.clj
+++ src/riemann/streams.clj
@@ -1519,7 +1519,7 @@
    (by-builder [host :host] (forward (get relay-by-host host)))
    "
   [[sym fields] & forms]
-  `(let [new-fork# (fn [~sym] [(do ~@forms)])]
+  `(let [new-fork# (fn [~sym] [~@forms])]
      (by-fn ~fields new-fork#)))
 
 (defn by-fn [fields new-fork]
