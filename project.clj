e70001f69e49fe04245b9660e895b08d117c6c51
mcorbin <corbin.math@gmail.com>
2017-12-16 22:23:13 +0100

    Update project dependencies

--- project.clj
+++ project.clj
@@ -10,40 +10,40 @@
     [org.clojure/algo.generic "0.1.2"]
     [org.clojure/clojure "1.8.0"]
     [org.clojure/math.numeric-tower "0.0.4"]
-    [org.clojure/tools.logging "0.3.1"]
-    [org.clojure/tools.nrepl "0.2.12"]
+    [org.clojure/tools.logging "0.4.0"]
+    [org.clojure/tools.nrepl "0.2.13"]
     [org.clojure/core.cache "0.6.5"]
     [org.clojure/data.priority-map "0.0.7"]
     [org.clojure/java.classpath "0.2.3"]
 
-    [org.slf4j/log4j-over-slf4j "1.7.14"]
-    [ch.qos.logback/logback-classic "1.1.3"]
+    [org.slf4j/log4j-over-slf4j "1.7.25"]
+    [ch.qos.logback/logback-classic "1.2.3"]
     [com.github.juise/logstash-logback-layout "1.0"]
-    [net.logstash.logback/logstash-logback-encoder "4.5"]
-    [org.influxdb/influxdb-java "2.7"]
+    [net.logstash.logback/logstash-logback-encoder "4.11"]
+    [org.influxdb/influxdb-java "2.8"]
     [com.cemerick/pomegranate "0.3.1"
      :exclusions [org.codehaus.plexus/plexus-utils]]
     ; for pomegranate
     [org.codehaus.plexus/plexus-utils "3.0"]
-    [http-kit "2.1.19"]
-    [clj-http "3.5.0"]
-    [cheshire "5.7.0"]
+    [http-kit "2.2.0"]
+    [clj-http "3.7.0"]
+    [cheshire "5.8.0"]
     [clj-librato "0.0.5"]
-    [clj-time "0.13.0"]
+    [clj-time "0.14.2"]
     [clj-wallhack "1.0.1"]
     [com.boundary/high-scale-lib "1.0.6"]
     [com.draines/postal "2.0.2"]
     [com.amazonaws/aws-java-sdk "1.11.116" :exclusions [joda-time]]
     [interval-metrics "1.0.0"]
     [io.netty/netty-all "4.1.15.Final"]
-    [clj-antlr "0.2.2"]
+    [clj-antlr "0.2.4"]
     [riemann-clojure-client "0.4.5"]
     [less-awful-ssl "1.0.1"]
     [slingshot "0.12.2"]
     [cljr-nsca "0.0.3"]
     [amazonica "0.3.95" :exclusions [joda-time]]
     [spootnik/kinsky "0.1.16"]
-    [pjstadig/humane-test-output "0.8.1"]]
+    [pjstadig/humane-test-output "0.8.3"]]
   :plugins [[lein-codox "0.10.2"]
             [lein-difftest "2.0.0"]
             [lein-rpm "0.0.6"
@@ -62,7 +62,7 @@
 ;                              "-Dcom.sun.management.jmxremote"
 ;                              "-XX:+UnlockCommercialFeatures"
 ;                              "-XX:+FlightRecorder"]
-                   :dependencies [[criterium "0.4.3"]]}}
+                   :dependencies [[criterium "0.4.4"]]}}
   :test-selectors {:default (fn [x] (not (or (:integration x)
                                              (:time x)
                                              (:bench x))))
