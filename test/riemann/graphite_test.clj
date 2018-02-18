dd9dd4ba8306156e7f595f0359e0314f774a2f2d
mcorbin <corbin.math@gmail.com>
2018-02-18 12:35:48 +0100

    Add multiple events support to the Graphite stream

--- test/riemann/graphite_test.clj
+++ test/riemann/graphite_test.clj
@@ -39,6 +39,34 @@
          (client/close! client)
          (stop! core))))))
 
+(deftest graphite-server-batch-test
+  (logging/suppress
+   ["riemann.transport" "riemann.core" "riemann.pubsub" "riemann.graphite"]
+   (let [s1       (graphite-server)
+         s2       (tcp-server)
+         index    (wrap-index (index/index))
+         core     (transition!
+                   (core)
+                   {:index    index
+                    :services [s1 s2]
+                    :streams  [index]})
+         sendout! (graphite {:path graphite-path-basic})
+         client   (client/tcp-client)]
+     (try
+       (sendout! [{:service "service1" :metric 1.0 :time 0}
+                  {:service "service2" :metric 1.0 :time 0}
+                  {:service "service3" :time 0}])
+       (Thread/sleep 100)
+       (let [[r1 r2 :as res] @(client/query client "true")]
+         (is (= (count res) 2))
+         (is (and (#{"service1" "service2"} (:service r1))
+                  (= 1.0 (:metric r1))))
+         (is (and (#{"service1" "service2"} (:service r2))
+                  (= 1.0 (:metric r2)))))
+       (finally
+         (client/close! client)
+         (stop! core))))))
+
 (deftest parse-error-test
   (logging/suppress
     ["riemann.transport" "riemann.core" "riemann.pubsub" "riemann.graphite"]
@@ -92,24 +120,35 @@
          (double 3.14159))))
 
 (deftest ^:graphite ^:integration graphite-test
-         (let [g (graphite {:block-start true})]
-           (g {:host "riemann.local"
-               :service "graphite test"
-               :state "ok"
-               :description "all clear, uh, situation normal"
-               :metric -2
-               :time (unix-time)}))
+  (let [g (graphite {:block-start true})]
+    (g {:host "riemann.local"
+        :service "graphite test"
+        :state "ok"
+        :description "all clear, uh, situation normal"
+        :metric -2
+        :time (unix-time)}))
 
-         (let [g (graphite {:block-start true})]
-           (g {:service "graphite test"
-               :state "ok"
-               :description "all clear, uh, situation normal"
-               :metric 3.14159
-               :time (unix-time)}))
+  (let [g (graphite {:block-start true})]
+    (g {:service "graphite test"
+        :state "ok"
+        :description "all clear, uh, situation normal"
+        :metric 3.14159
+        :time (unix-time)}))
 
-         (let [g (graphite {:block-start true})]
-           (g {:host "no-service.riemann.local"
-               :state "ok"
-               :description "all clear, uh, situation normal"
-               :metric 4
-               :time (unix-time)})))
+  (let [g (graphite {:block-start true})]
+    (g {:host "no-service.riemann.local"
+        :state "ok"
+        :description "all clear, uh, situation normal"
+        :metric 4
+        :time (unix-time)}))
+  (let [g (graphite {:block-start true})]
+    (g [{:host "no-service.riemann.local"
+         :state "ok"
+         :description "all clear, uh, situation normal"
+         :metric 4
+         :time (unix-time)}
+        {:host "no-service.riemann.local"
+         :state "ok"
+         :description "all clear, uh, situation normal"
+         :metric 10
+         :time (unix-time)}])))
