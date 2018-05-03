50c498846d8002b8483e842c52e035efb9774444
mcorbin <corbin.math@gmail.com>
2018-05-03 21:59:46 +0200

    Support for Opsgenie v2 API

--- src/riemann/opsgenie.clj
+++ src/riemann/opsgenie.clj
@@ -3,21 +3,22 @@
   (:require [clj-http.client :as client])
   (:require [cheshire.core :as json]))
 
-(def ^:private alerts-url
-  "https://api.opsgenie.com/v1/json/alert")
+(def alerts-url
+  "https://api.opsgenie.com/v2/alerts")
 
-(defn- post
- "Post to OpsGenie"
- [url body]
+(defn post
+  "Post to OpsGenie"
+  [url body headers]
   (client/post url
-                {:body body
-                 :socket-timeout 5000
-                 :conn-timeout 5000
-                 :content-type :json
-                 :accept :json
-                 :throw-entire-message? true}))
+               {:body body
+                :headers headers
+                :socket-timeout 5000
+                :conn-timeout 5000
+                :content-type :json
+                :accept :json
+                :throw-entire-message? true}))
 
-(defn- message
+(defn message
   "Generate description based on event.
   Because service might be quite long and opsgenie limits message, it
   pulls more important info into beginning of the string"
@@ -26,7 +27,7 @@
        ": [" (:state event) "] "
        (:service event)))
 
-(defn- description
+(defn description
   "Generate message based on event"
   [event]
   (str
@@ -36,41 +37,58 @@
    " \nMetric: " (:metric event)
    " \nDescription: " (:description event)))
 
-(defn- api-alias
+(defn api-alias
   "Generate OpsGenie alias based on event"
   [event]
   (hash (str (:host event) \uffff (:service event) \uffff
        (clojure.string/join \uffff (sort (:tags event))))))
 
-(defn- create-alert
+(defn default-body
+  [event]
+  {:message (message event)
+   :description (description event)
+   :alias (api-alias event)
+   :user "Riemann"
+   :tags (:tags event [])})
+
+(defn create-alert
   "Create alert in OpsGenie"
-  [api-key event recipients]
-  (post alerts-url (json/generate-string
-                    {:message (message event)
-                     :description (description event)
-                     :apiKey api-key
-                     :alias (api-alias event)
-                     :tags (clojure.string/join "," (:tags event))
-                     :recipients recipients})))
-(defn- close-alert
+  [api-key event body-fn]
+  (post alerts-url
+        (json/generate-string (body-fn event))
+        {"Authorization" (str "GenieKey " api-key)}))
+
+(defn close-alert
   "Close alert in OpsGenie"
-  [api-key event]
-  (post (str alerts-url "/close")
-        (json/generate-string
-          {:apiKey api-key
-           :alias (api-alias event)})))
+  [api-key event body-fn]
+  (post (str alerts-url "/" (:alias (body-fn event)) "/close?identifierType=alias")
+        (json/generate-string {:user "Riemann"})
+        {"Authorization" (str "GenieKey " api-key)}))
+
+(def default-options
+  {:body-fn default-body})
 
 (defn opsgenie
-  "Creates an OpsGenie adapter. Takes your OG service key, and returns a map of
-  functions which trigger and resolve events. clojure/hash from event host, service and tags
-  will be used as the alias.
+  "Creates an OpsGenie adapter. Takes options, and returns a map of
+  functions which trigger and resolve events.
+  By default, Clojure/hash from event host, service and tags will be used as
+  the alias.
+
+  Options:
+
+  - :api-key    Your Opsgenie API key
+  - :body-fn    A function used to generate the HTTP request body. default to
+  `default-body`. This function should accept an event and return a map
+  containing at least the `:message` and `:alias` keys.
 
   ```clojure
-  (let [og (opsgenie \"my-service-key\" \"recipient@example.com\")]
+  (let [og (opsgenie {:api-key \"my-api-key\"})]
     (changed-state
       (where (state \"ok\") (:resolve og))
       (where (state \"critical\") (:trigger og))))
   ```"
-  [service-key recipients]
-  {:trigger     #(create-alert service-key % recipients)
-   :resolve     #(close-alert service-key %)})
+  [options]
+  (assert (:api-key options))
+  (let [{:keys [api-key body-fn]} (merge default-options options)]
+    {:trigger     #(create-alert api-key % body-fn)
+     :resolve     #(close-alert api-key % body-fn)}))
