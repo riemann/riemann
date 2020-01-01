4352c53680d7ce905fad9de2b33b47f31f05f26c
Serge Medvedev <serge-medvedev@users.noreply.github.com>
2020-01-01 18:55:23 +0200

    feat(transport): rabbitmq-transport got index querying support (#963)
    * feat(transport): rabbitmq-transport got index querying support
    
    * test(transport): index querying via rabbitmq is tested

--- test/riemann/transport/rabbitmq_test.clj
+++ test/riemann/transport/rabbitmq_test.clj
@@ -1,12 +1,15 @@
 (ns riemann.transport.rabbitmq-test
-  (:require [riemann.common :refer [encode event map-matches?]]
+  (:require [riemann.common :refer [event map-matches?]]
             [riemann.config :refer [service!]]
             [riemann.core :as core]
+            [riemann.index :as index]
             [riemann.transport.rabbitmq :refer :all]
             [clojure.test :refer :all]
             [langohr.core :as rmq]
+            [langohr.queue :as lq]
             [langohr.channel :as lch]
             [langohr.exchange :as le]
+            [langohr.consumers :as lc]
             [langohr.basic :as lb]))
 
 (def ^:const events
@@ -21,8 +24,14 @@
          :description "meow"
          :metric 9.0}})
 
+(defn- gen-query-result-handler
+  [query-result]
+  (fn [ch {:keys [correlation-id] :as meta} ^bytes payload]
+    (deliver query-result {:message (pb->msg payload)
+                           :meta meta})))
+
 (deftest ^:rabbitmq valid-protobuf-payload-decodes-into-a-message
-  (let [pb (encode {:events (map event (vals events))})
+  (let [pb (msg->pb {:events (map event (vals events))})
         result (pb->msg pb)
         [e1 e2] (:events result)]
     (is (map-matches? (:bee events) e1))
@@ -37,23 +46,37 @@
           ex-name "riemann-test"
           ex-type "topic"
           routing-key "test"
+          reply-to "queries"
           transport (rabbitmq-transport {:host host
                                          :ex-name ex-name
                                          :ex-type ex-type
                                          :routing-key routing-key})
+          index (core/wrap-index (index/index))
           sink (promise)
-          core (core/transition! (core/core) {:services [transport]
-                                              :streams [(partial deliver sink)]})
+          query-result (promise)
+          core (core/transition! (core/core) {:index index
+                                              :services [transport]
+                                              :streams [index (partial deliver sink)]})
           conn (atom nil)
           ch (atom nil)
-          payload (encode {:events [(event (:bee events))]})]
+          payload (msg->pb {:events [(event (:bee events))]
+                            :query {:string "service = \"busybee\""}})]
       (try
         (reset! conn (rmq/connect {:host host}))
         (reset! ch (lch/open @conn))
         (le/declare @ch ex-name ex-type {:durable false :auto-delete true})
+        (let [q-name (.getQueue (lq/declare @ch "" {:exclusive false :auto-delete true}))]
+          (lq/bind @ch q-name ex-name {:routing-key reply-to})
+          (lc/subscribe @ch q-name (gen-query-result-handler query-result) {:auto-ack true}))
         (lb/publish @ch ex-name routing-key payload {:content-type "application/octet-stream"
+                                                     :reply-to reply-to
+                                                     :correlation-id "hi"
                                                      :type "riemann.test"})
         (is (map-matches? (:bee events) (deref sink 1000 :timed-out)))
+        (let [{message :message meta :meta} (deref query-result 1000 :timed-out)]
+          (is (= (:correlation-id meta) "hi"))
+          (is (map-matches? {:ok true :error nil} message))
+          (is (map-matches? (:bee events) (-> message :events (first)))))
         (finally
           (core/stop! core)
           (when @conn
