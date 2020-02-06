702c97803d2c29f91694d0361fd6e67ad16e9d1f
Serge Medvedev <thismoment.main@gmail.com>
2020-02-07 00:20:00 +0200

    docs(stream): rabbitmq stream comments are tidied up

--- src/riemann/rabbitmq.clj
+++ src/riemann/rabbitmq.clj
@@ -20,9 +20,24 @@
 
   Options:
 
-  - :exchange-settings Settings an exchange is declared with, defaults are {:name \"riemann\" :type \"topic\" :durable false :auto-delete false :internal false}.
+  - :exchange-settings Settings an exchange is declared with, defaults are:
+
+  ```clojure
+  {:name \"riemann\"
+   :type \"topic\"
+   :durable false
+   :auto-delete false
+   :internal false}
+  ```
+
   - :routing-key Routing key messages to be published with, default is \"riemann.events\".
-  - :message-properties Properties of messages to publish, defaults are {:content-type \"application/json\" :mandatory false}.
+  - :message-properties Properties of messages to publish, defaults are:
+  
+  ```clojure
+  {:content-type \"application/json\"
+   :mandatory false}
+  ```
+
   - :message-formatter A function to format event(s), default format is JSON.
 
   ```clojure
@@ -34,9 +49,9 @@
           :routing-key \"riemann.events.hello\"}))
   ```
 
-  For details on exchange declaration options and message properties refer to Langohr API reference for
-    - [declare](http://reference.clojurerabbitmq.info/langohr.exchange.html#var-declare) and
-    - [publish](http://reference.clojurerabbitmq.info/langohr.basic.html#var-publish)
+  For details on exchange declaration options and message properties refer to Langohr API reference for 
+  [declare](http://reference.clojurerabbitmq.info/langohr.exchange.html#var-declare) and 
+  [publish](http://reference.clojurerabbitmq.info/langohr.basic.html#var-publish).
   "
   ([] (rabbitmq {}))
   ([opts]
