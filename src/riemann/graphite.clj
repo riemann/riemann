b4b54efd83b152c4adedbdac699ce5a2badb2bed
James Turnbull <james@lovedthanlost.net>
2025-02-17 07:55:32 +0700

    Fixed when-not form is missing a body so that it has an expression for true

--- src/riemann/graphite.clj
+++ src/riemann/graphite.clj
@@ -79,11 +79,11 @@
   (graphite-path-basic
     (if-let [service (:service event)]
       (assoc event :service
-             ; hack hack hack hack
              (replace service
                       #"(\d+)\.(\d+)$"
-                      (fn [[_ whole frac]] (str (when-not (= "0" whole))
-                                                frac))))
+                      (fn [[_ whole frac]]
+                        (str (when-not (= "0" whole) whole)
+                             frac))))
       event)))
 
 (defn graphite-path-tags
